1cde4b0e6d8aa479166a086cdc597374f357b972
MyNameIsAres <chrisvonk@hotmail.nl>
2021-11-11 12:11:24 +0100

    Created first folder to show my solutio for a simple code problem.

---
+++ match_results/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var input string
+	fmt.Scanln(&input)
+
+	results := []string{"w", "l", "w", "d", "w", "l", "l", "l", "d", "d", "w", "l", "w", "d"}
+	results = append(results, input)
+
+	fmt.Println(scanMatchResults(results...))
+}
+
+func scanMatchResults(results ...string) int {
+	total := 0
+	for _, char := range results {
+		total += determinePoints(char)
+	}
+	return total
+}
+
+func determinePoints(char string) int {
+	if char == "w" {
+		return 3
+	} else if char == "d" {
+		return 1
+	}
+	return 0
+}
